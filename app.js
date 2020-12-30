2515588e7d1817d13cbbfdc80e456da3a227ef19
unknown <cba5530@gmail.com>
2020-12-31 01:15:20 +0800

    refactor:modify mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const Expense = require('./models/expense')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override') 
 const routes = require('./routes')
+require('./config/mongoose')
 
 const PORT = 3000
 
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true,  useUnifiedTopology: true})
-const db = mongoose.connection
-db.on('error', () => {
-    console.log('mongodb error!')
-})
 
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
@@ -29,6 +21,6 @@ app.use(routes)
 //路由
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('App is running on http://localhost:3000')
 })
