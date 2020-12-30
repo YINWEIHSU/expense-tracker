5606927a1de191538dfe24bc3afc44f04167b90d
unknown <cba5530@gmail.com>
2020-12-30 23:15:58 +0800

    feat: add create expense function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const Expense = require('./models/expense')
+const bodyParser = require('body-parser')
 
 const PORT = 3000
 
@@ -19,12 +20,21 @@ db.once('open', () => {
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //路由
 app.get('/', (req, res) => {
     Expense.find().lean().then( expenses => res.render('index', {expenses}))
     .catch(error => console.error(error))
 })
-
+app.get('/expenses/new', (req,res) => {
+    return res.render('new')
+})
+app.post('/expenses', (req, res) => {
+    return Expense.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
