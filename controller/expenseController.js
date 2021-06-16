21064b4af19798c866d04358e8be16c110918c4d
YinWei <cba5530@gmail.com>
2021-06-16 15:54:22 +0800

    rebuild: expenseController

---
+++ controller/expenseController.js
@@ -0,0 +1,40 @@
+const Record = require('../models/record')
+
+const expenseController = {
+  createExpense: (req, res) => {
+    return res.render('new')
+  },
+  editExpense: (req, res) => {
+    const userId = req.user._id
+    const _id = req.params.id
+    return Record.findOne({ _id, userId }).lean()
+      .then(expense => res.render('edit', { expense }))
+      .catch(error => console.log(error))
+  },
+  postExpense: (req, res) => {
+    req.body.userId = req.user._id
+    return Record.create(req.body)
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+  },
+  putExpense: (req, res) => {
+    const userId = req.user._id
+    const _id = req.params.id
+    return Record.findOne({ _id, userId })
+      .then(expense => {
+        expense = Object.assign(expense, req.body)
+        return expense.save()
+      })
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+  },
+  deleteExpense: (req, res) => {
+    const userId = req.user._id
+    const _id = req.params.id
+    return Record.findOne({ _id, userId })
+      .then(expense => expense.remove())
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+  }
+}
+module.exports = expenseController
--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -1,42 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const expenseController = require('../../controller/expenseController')
 
-const Record = require('../../models/record')
-
-router.get('/new', (req, res) => {
-  return res.render('new')
-})
-router.get('/:id/edit', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  return Record.findOne({ _id, userId }).lean()
-    .then(expense => res.render('edit', { expense }))
-    .catch(error => console.log(error))
-})
-router.post('/', (req, res) => {
-  req.body.userId = req.user._id
-  return Record.create(req.body)
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-router.put('/:id', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  return Record.findOne({ _id, userId })
-    .then(expense => {
-      expense = Object.assign(expense, req.body)
-      return expense.save()
-    })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-router.delete('/:id', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  return Record.findOne({ _id, userId })
-    .then(expense => expense.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+router.get('/new', expenseController.createExpense)
+router.get('/:id/edit', expenseController.editExpense)
+router.post('/', expenseController.postExpense)
+router.put('/:id', expenseController.putExpense)
+router.delete('/:id', expenseController.deleteExpense)
 
 module.exports = router
