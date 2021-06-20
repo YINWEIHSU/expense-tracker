f26a3a7d93d2f0e6405d5b571fc374d1d352fe1a
YinWei <cba5530@gmail.com>
2021-06-20 20:43:03 +0800

    hotfix:number display

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -17,7 +17,7 @@ const recordList = [
         name: '早餐',
         category: '飲食',
         date: '2021-1-20',
-        amount: 54,
+        amount: -54,
         remarks: ''
     },
     {
@@ -25,7 +25,7 @@ const recordList = [
         name: '高鐵',
         category: '交通',
         date: '2021-1-29',
-        amount: 1490,
+        amount: -1490,
         remarks: ''
     },
     {
@@ -33,7 +33,7 @@ const recordList = [
         name: '靈魂急轉彎',
         category: '娛樂',
         date: '2021-1-1',
-        amount: 320,
+        amount: -320,
         remarks: ''
     },
     {
@@ -41,7 +41,7 @@ const recordList = [
         name: '蟑螂藥',
         category: '生活',
         date: '2021-2-28',
-        amount: 150,
+        amount: -150,
         remarks: ''
     },
     {
@@ -49,18 +49,19 @@ const recordList = [
         name: '股票',
         category: '其他',
         date: '2021-3-4',
-        amount: 450000,
+        amount: -45000,
+        remarks: ''
+    },
+    {
+        type: 'income',
+        name: '薪資',
+        category: '其他',
+        date: '2021-3-20',
+        amount: 50000,
         remarks: ''
     }
 ]
 
-// db.once('open', () => {
-//     console.log('mongodb connected!')
-//     recordList.forEach(record => Record.create(record).then(() => {
-//         console.log('Insert seed')
-//     }))
-//     setTimeout(() => db.close(), 1000)
-// })
 
 db.once('open', () => {
     bcrypt
@@ -84,10 +85,4 @@ db.once('open', () => {
             process.exit()
         })
         .catch(error => console.warn('error', error))
-    //     Record.create(recordList).then(() => {
-    //         console.log('insert record data done...')
-    //         return db.close()
-    //     }).then(() => {
-    //         console.log('database connection closed...')
-    //     })
 })
